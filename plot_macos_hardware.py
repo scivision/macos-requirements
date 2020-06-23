89b0313610b05d9f91d6460f5eb475662052dca4
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2020-06-23 00:33:14 -0400

    init

---
+++ plot_macos_hardware.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+import pandas
+from matplotlib.pyplot import figure, show, xkcd
+from pathlib import Path
+
+R = Path(__file__).parent
+
+fn = R / "MacOShardware.csv"
+
+dat = pandas.read_csv(fn, index_col=0, usecols=[0, 1, 2, 3, 4, 5, 6])
+dat.index = dat.index.str.rstrip("0")
+
+with xkcd():
+    fg = figure(figsize=(12, 5))
+    axs = fg.subplots(1, 2, sharey=True)
+
+    ax = axs[0]
+    for k in ("Macbook", "Macbook Air", "Macbook Pro"):
+        ax.plot(dat.index, dat[k].values, label=k, alpha=0.5, linestyle="--", marker="*")
+    ax.tick_params(axis="x", labelrotation=20)
+    ax.set_ylabel("year")
+    ax.set_xlabel("MacOS version")
+    ax.legend()
+
+    ax = axs[1]
+    for k in ("Mac Mini", "Mac Pro", "iMac"):
+        ax.plot(dat.index, dat[k].values, label=k, alpha=0.5, linestyle="--", marker="*")
+    ax.tick_params(axis="x", labelrotation=20)
+    ax.legend()
+
+    fg.suptitle("MacOS version vs. hardware requirements")
+
+show()
