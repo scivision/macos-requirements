67d065032662448a0441fadd5c92c599e6e7b8ab
scivision <scivision@users.noreply.github.com>
2024-02-10 19:13:54 -0500

    lint

--- plot_macos_hardware.py
+++ plot_macos_hardware.py
@@ -1,32 +1,35 @@
 #!/usr/bin/env python3
+
+import typing
+from pathlib import Path
+
 import pandas
 from matplotlib.pyplot import figure, show, xkcd
-from pathlib import Path
 
 R = Path(__file__).parent
 
-fn = R / "MacOShardware.csv"
+fn = R / "macOShardware.csv"
 
 dat = pandas.read_csv(fn, index_col=0, usecols=[0, 2, 3, 4, 5, 6], skiprows=(1, 2, 3))
 
 with xkcd():
     fg = figure(figsize=(12, 5))
-    axs = fg.subplots(1, 2, sharey=True)
+    axs: typing.Any = fg.subplots(1, 2, sharey=True)
 
     ax = axs[0]
-    for k in ("Macbook Air", "Macbook Pro"):
+    for k in {"Macbook Air", "Macbook Pro"}:
         ax.plot(dat.index, dat[k].values, label=k, alpha=0.5, linestyle="--", marker="*")
     ax.tick_params(axis="x", labelrotation=20)
     ax.set_ylabel("year")
-    ax.set_xlabel("MacOS version")
+    ax.set_xlabel("macOS version")
     ax.legend()
 
     ax = axs[1]
-    for k in ("Mac Mini", "Mac Pro", "iMac"):
+    for k in {"Mac Mini", "Mac Pro", "iMac"}:
         ax.plot(dat.index, dat[k].values, label=k, alpha=0.5, linestyle="--", marker="*")
     ax.tick_params(axis="x", labelrotation=20)
     ax.legend()
 
-    fg.suptitle("MacOS version vs. hardware requirements")
+    fg.suptitle("macOS version vs. hardware required")
 
 show()
