a80f6dd89f0f55f9280f7459df162e4396c667e0
scivision <scivision@users.noreply.github.com>
2025-06-12 19:14:34 -0400

    improve plots

--- plot_macos_hardware.py
+++ plot_macos_hardware.py
@@ -4,7 +4,8 @@ import typing
 from pathlib import Path
 
 import pandas
-from matplotlib.pyplot import figure, show, xkcd
+import matplotlib.pyplot as plt
+from matplotlib.ticker import MaxNLocator
 
 R = Path(__file__).parent
 
@@ -12,8 +13,8 @@ fn = R / "macOShardware.csv"
 
 dat = pandas.read_csv(fn, index_col=0, skiprows=(1, 2, 3, 4, 5))
 
-with xkcd():
-    fg = figure(figsize=(12, 5))
+with plt.xkcd():
+    fg = plt.figure(figsize=(12, 5), layout="constrained")
     axs: typing.Any = fg.subplots(1, 2, sharey=True)
 
     ax = axs[0]
@@ -22,16 +23,6 @@ with xkcd():
     ax.tick_params(axis="x", labelrotation=20)
     ax.set_ylabel("year")
     ax.set_xlabel("macOS version")
-    ax.plot(
-        dat.index,
-        dat["EOL"].values,
-        label="macOS EOL",
-        alpha=0.5,
-        linestyle="--",
-        marker="*",
-        color="red",
-    )
-
     ax.legend()
 
     ax = axs[1]
@@ -42,4 +33,19 @@ with xkcd():
 
     fg.suptitle("macOS version vs. hardware required")
 
-show()
+    fg = plt.figure(constrained_layout=True)
+    ax = fg.gca()
+    ax.set_ylabel("year")
+    ax.set_xlabel("macOS version")
+    ax.set_title("macOS version vs. End of Life")
+    ax.yaxis.set_major_locator(MaxNLocator(integer=True))
+
+    ax.plot(
+        dat.index,
+        dat["EOL"].values,
+        linestyle="--",
+        marker="*",
+        color="red",
+    )
+
+plt.show()
